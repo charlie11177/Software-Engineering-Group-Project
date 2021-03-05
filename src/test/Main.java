5df4d5638a74296457e2b757db753e817a6f0632
Tomas Mrkva <22m.tomash@gmail.com>
2021-03-05 18:16:52 +0000

    UI classes + FXML files added

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,7 +1,15 @@
 package test;
 
-class HelloWorld {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
