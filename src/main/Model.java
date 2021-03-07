1522f439c2ea4c3d48ef98931fc7402ad83053b2
Tomas Mrkva <22m.tomash@gmail.com>
2021-03-07 23:14:45 +0000

    Removed out + added comments

--- src/main/Model.java
+++ src/main/Model.java
@@ -2,9 +2,14 @@ package main;
 
 import main.controllers.CenterScreenController;
 
+/**
+ * Model class used as the main class that holds all data from classes that represent Airport, Runway, Obstacles, etc.
+ * Used for communicating between Controller and Model classes
+ */
 public class Model {
 
+    // console that holds all the log texts to display
     public static Console console = new Console();
+    // Center of the UI, main visualisation window and console textArea
     public static CenterScreenController centerScreenController;
-
 }
--- src/main/controllers/MainWindowController.java
+++ src/main/controllers/MainWindowController.java
@@ -3,6 +3,7 @@ package main.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 
+
 public class MainWindowController {
     @FXML private LeftScreenController leftScreenController;
     @FXML private CenterScreenController centerScreenController;
