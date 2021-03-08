c33902cd1b60bb375964c0b12db5bb653d07c3f6
jm10g19 <jm10g19@soton.ac.uk>
2021-03-08 20:27:37 +0000

    XML import get airport name

--- src/main/XMLParser.java
+++ src/main/XMLParser.java
@@ -2,18 +2,26 @@ package main;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class XMLParser {
-    private DocumentBuilderFactory documentBuilderFactory;
+    private final DocumentBuilderFactory documentBuilderFactory;
 
     public XMLParser() {
         documentBuilderFactory = DocumentBuilderFactory.newInstance();
+    }
+
+    public static void main(String[] args) {
+        XMLParser xml = new XMLParser();
+        xml.importAirport("C:\\Dev\\Java\\comp2211-airport\\src\\main\\xml\\airport1.xml");
     }
 
     /*
@@ -22,11 +30,22 @@ public class XMLParser {
     */
     public Airport importAirport(String filename) {
         Airport airport = null;
+        List<PhysicalRunWay> runways = null;
         try {
+            // Boiler plate code for creating an XML parser
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(new File(filename));
-            Element root = document.getDocumentElement();
-            System.out.println(root);
+            document.getDocumentElement().normalize();
+
+            /*
+            Get name of the aiport
+            first check that there is only one airport tag (should be root element) then get child and contents
+            */
+            NodeList airportList = document.getElementsByTagName("airport");
+            if (airportList.getLength() != 1) throw new Exception("Airport tag not found in XML file");
+            Node airportElement = airportList.item(0);
+            String name = airportElement.getChildNodes().item(1).getTextContent();
+
         } catch(Exception e) {
             e.printStackTrace();
         }
