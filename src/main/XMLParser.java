13e4e0f09a216233de3ab5377074cd9a068c853e
jm10g19 <jm10g19@soton.ac.uk>
2021-03-09 15:07:23 +0000

    Changed airport xml import to have multiple airports per xml file, rather than just one

--- src/main/XMLParser.java
+++ src/main/XMLParser.java
@@ -8,8 +8,8 @@ import org.w3c.dom.Node;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class XMLParser {
@@ -21,23 +21,25 @@ public class XMLParser {
 
     public static void main(String[] args) {
         XMLParser xml = new XMLParser();
-        Airport testAirport = xml.importAirport("src/main/xml/airport1.xml");
+        ArrayList<Airport> testAirports = xml.importAirports("src/main/xml/airportsimport.xml");
         Obstacle testObstacle = xml.importObstacle("src/main/xml/obstacle1.xml");
-        System.out.println(testAirport.getName());
-        System.out.println(testAirport.getRunways());
-        System.out.println(testObstacle.getName());
-        System.out.println(testObstacle.getHeight());
-        System.out.println(testObstacle.getWidth());
-        System.out.println(testObstacle.getPosition());
+
+        Iterator iter = testAirports.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next().toString());
+        }
+        System.out.println();
+
     }
 
     /*
      *  Currently using a string to parse in
      *  TODO change to File input and use JavaFX explorer window probably
     */
-    public Airport importAirport(String filename) {
+    public ArrayList<Airport> importAirports(String filename) {
         Airport airport = null;
         List<PhysicalRunWay> runways = new ArrayList<PhysicalRunWay>();
+        ArrayList<Airport> airports = new ArrayList<Airport>();
         try {
             // Boiler plate code for creating an XML parser
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
@@ -45,57 +47,64 @@ public class XMLParser {
             document.getDocumentElement().normalize();
 
             /*
-            Get name of the aiport
-            first check that there is only one airport tag (should be root element) then get child and contents
-            */
-            NodeList airportList = document.getElementsByTagName("airport");
-            if (airportList.getLength() != 1) throw new Exception("Airport tag not found in XML file");
-            Node airportElement = airportList.item(0);
-            String name = airportElement.getChildNodes().item(1).getTextContent();
-
-            /*
-            Get runway information
+             * Get the list of airports
              */
-            NodeList runwayNodes = document.getElementsByTagName("runway");
-            // Iterate over runways
-            for (int i = 0; i < runwayNodes.getLength(); i++) {
-                // Gets current runway from list
-                Element runway = (Element) runwayNodes.item(i);
+            NodeList airportList = document.getElementsByTagName("airport");
+            if (airportList.getLength() == 0) throw new Exception("Airport tag not found in XML file");
+            for (int i = 0; i < airportList.getLength(); i++) {
 
-                // Gets the runway ID
-                int ID = Integer.parseInt(runway.getElementsByTagName("ID").item(0).getTextContent());
                 /*
-                 Gets the runway attributes for each logical runway
-                 Assumes left runway is always first child
-                 */
+                Get name of the aiport
+                first check that there is only one airport tag (should be root element) then get child and contents
+                */
+                Node airportElement = airportList.item(i);
+                String name = airportElement.getChildNodes().item(1).getTextContent();
 
-                //Name of the runway was changed to a degree
-                int degree_L = Integer.parseInt(runway.getElementsByTagName("degree").item(0).getTextContent());
-                int degree_R = Integer.parseInt(runway.getElementsByTagName("degree").item(1).getTextContent());
+                /*
+                *Get runway information
+                */
+                NodeList runwayNodes = document.getElementsByTagName("runway");
+                // Iterate over runways
+                for (int j = 0; j < runwayNodes.getLength(); j++) {
+                    // Gets current runway from list
+                    Element runway = (Element) runwayNodes.item(j);
 
-                int TORA_L = Integer.parseInt(runway.getElementsByTagName("TORA").item(0).getTextContent());
-                int TORA_R = Integer.parseInt(runway.getElementsByTagName("TORA").item(1).getTextContent());
+                    // Gets the runway ID
+                    int ID = Integer.parseInt(runway.getElementsByTagName("ID").item(0).getTextContent());
 
-                int TODA_L = Integer.parseInt(runway.getElementsByTagName("TODA").item(0).getTextContent());
-                int TODA_R = Integer.parseInt(runway.getElementsByTagName("TODA").item(1).getTextContent());
+                    /*
+                     Gets the runway attributes for each logical runway
+                     Assumes left runway is always first child
+                     */
 
-                int ASDA_L = Integer.parseInt(runway.getElementsByTagName("ASDA").item(0).getTextContent());
-                int ASDA_R = Integer.parseInt(runway.getElementsByTagName("ASDA").item(1).getTextContent());
+                    //Name of the runway was changed to a degree
+                    int degree_L = Integer.parseInt(runway.getElementsByTagName("degree").item(0).getTextContent());
+                    int degree_R = Integer.parseInt(runway.getElementsByTagName("degree").item(1).getTextContent());
 
-                int LDA_L = Integer.parseInt(runway.getElementsByTagName("LDA").item(0).getTextContent());
-                int LDA_R = Integer.parseInt(runway.getElementsByTagName("LDA").item(1).getTextContent());
+                    int TORA_L = Integer.parseInt(runway.getElementsByTagName("TORA").item(0).getTextContent());
+                    int TORA_R = Integer.parseInt(runway.getElementsByTagName("TORA").item(1).getTextContent());
 
-                //Direction has been changed here from LogicalRunway.Direction.Left to Direction.Left
-                LogicalRunWay leftRunway = new LogicalRunWay(degree_L, Direction.L, TORA_L, TODA_L, ASDA_L, LDA_L);
-                LogicalRunWay rightRunway = new LogicalRunWay(degree_R, Direction.R, TORA_R, TODA_R, ASDA_R, LDA_R);
-                runways.add(new PhysicalRunWay(ID, leftRunway, rightRunway, null));
+                    int TODA_L = Integer.parseInt(runway.getElementsByTagName("TODA").item(0).getTextContent());
+                    int TODA_R = Integer.parseInt(runway.getElementsByTagName("TODA").item(1).getTextContent());
+
+                    int ASDA_L = Integer.parseInt(runway.getElementsByTagName("ASDA").item(0).getTextContent());
+                    int ASDA_R = Integer.parseInt(runway.getElementsByTagName("ASDA").item(1).getTextContent());
+
+                    int LDA_L = Integer.parseInt(runway.getElementsByTagName("LDA").item(0).getTextContent());
+                    int LDA_R = Integer.parseInt(runway.getElementsByTagName("LDA").item(1).getTextContent());
+
+                    //Direction has been changed here from LogicalRunway.Direction.Left to Direction.Left
+                    LogicalRunWay leftRunway = new LogicalRunWay(degree_L, Direction.L, TORA_L, TODA_L, ASDA_L, LDA_L);
+                    LogicalRunWay rightRunway = new LogicalRunWay(degree_R, Direction.R, TORA_R, TODA_R, ASDA_R, LDA_R);
+                    runways.add(new PhysicalRunWay(ID, leftRunway, rightRunway, null));
+                }
+                //Airport has additional "code" value like LHR for London Heathrow
+                airports.add(new Airport(name, null ,runways));
             }
-            //Aiport has additional "code" value like LHR for London Heathrow
-           airport = new Airport(name, null ,runways);
         } catch(Exception e) {
             e.printStackTrace();
         }
-        return airport;
+        return airports;
     }
 
     public Obstacle importObstacle(String filename) {
