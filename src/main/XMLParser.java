4635d19534ef6eed054683eedb6d59a4233274a5
Isaac <iw3g19@soton.ac.uk>
2021-03-09 04:19:21 +0000

    Added Obstacle Import Removed Notification Window Class (JavaFx alert already exists)

--- src/main/XMLParser.java
+++ src/main/XMLParser.java
@@ -21,7 +21,14 @@ public class XMLParser {
 
     public static void main(String[] args) {
         XMLParser xml = new XMLParser();
-        xml.importAirport("C:\\Dev\\Java\\comp2211-airport\\src\\main\\xml\\airport1.xml");
+        Airport testAirport = xml.importAirport("src/main/xml/airport1.xml");
+        Obstacle testObstacle = xml.importObstacle("src/main/xml/obstacle1.xml");
+        System.out.println(testAirport.getName());
+        System.out.println(testAirport.getRunways());
+        System.out.println(testObstacle.getName());
+        System.out.println(testObstacle.getHeight());
+        System.out.println(testObstacle.getWidth());
+        System.out.println(testObstacle.getPosition());
     }
 
     /*
@@ -30,7 +37,7 @@ public class XMLParser {
     */
     public Airport importAirport(String filename) {
         Airport airport = null;
-        List<PhysicalRunWay> runways = null;
+        List<PhysicalRunWay> runways = new ArrayList<PhysicalRunWay>();
         try {
             // Boiler plate code for creating an XML parser
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
@@ -51,7 +58,6 @@ public class XMLParser {
              */
             NodeList runwayNodes = document.getElementsByTagName("runway");
             // Iterate over runways
-            System.out.println(runwayNodes.getLength());
             for (int i = 0; i < runwayNodes.getLength(); i++) {
                 // Gets current runway from list
                 Element runway = (Element) runwayNodes.item(i);
@@ -93,7 +99,36 @@ public class XMLParser {
     }
 
     public Obstacle importObstacle(String filename) {
-        return null;
+        Obstacle obstacle = null;
+        try {
+            // Boiler plate code for creating an XML parser
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(new File(filename));
+            document.getDocumentElement().normalize();
+
+            /*
+            Get name of the obstacle
+            first check that there is only one obstacle tag (should be root element) then get child and contents
+            */
+            NodeList obstacleList = document.getElementsByTagName("obstacle");
+            if (obstacleList.getLength() != 1) throw new Exception("Obstacle tag not found in xml file");
+            Element obstacleElement = (Element) obstacleList.item(0);
+
+            String name = obstacleElement.getChildNodes().item(1).getTextContent();
+
+            int height = Integer.parseInt(obstacleElement.getElementsByTagName("height").item(0).getTextContent());
+            int width = Integer.parseInt(obstacleElement.getElementsByTagName("width").item(0).getTextContent());
+
+            Element positionNode = (Element) obstacleElement.getElementsByTagName("position").item(0);
+            int distanceToLeft = Integer.parseInt(positionNode.getElementsByTagName("distanceToLeft").item(0).getTextContent());
+            int distanceToRight = Integer.parseInt(positionNode.getElementsByTagName("distanceToRight").item(0).getTextContent());
+
+            Position position = new Position(distanceToLeft, distanceToRight);
+            obstacle = new Obstacle(name, height, width, position);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return obstacle;
     }
 
     public void exportAirport(String filename) {}
