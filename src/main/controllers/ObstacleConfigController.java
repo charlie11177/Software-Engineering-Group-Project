3df8ffd4f33f18b748f5e1066ca3b0dc0fe39f4d
Tomas Mrkva <22m.tomash@gmail.com>
2021-03-09 04:18:51 +0000

    Obstacle UI setup methods

--- src/main/controllers/ObstacleConfigController.java
+++ src/main/controllers/ObstacleConfigController.java
@@ -1,4 +1,12 @@
 package main.controllers;
 
+import javafx.event.ActionEvent;
+
 public class ObstacleConfigController {
+
+    public void newObstacleButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void editObstacleButtonClick(ActionEvent actionEvent) {
+    }
 }
