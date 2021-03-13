c07b5dc49c20311217f1efc872b6acc7cabd282a
Tomas Mrkva <22m.tomash@gmail.com>
2021-03-13 00:15:15 +0000

    AirportConfig workds

--- src/main/controllers/MainWindowController.java
+++ src/main/controllers/MainWindowController.java
@@ -16,86 +16,18 @@ import java.util.ArrayList;
 
 
 public class MainWindowController {
-    @FXML private LeftScreenController leftScreenController;
-    @FXML private CenterScreenController centerScreenController;
-    @FXML private RightScreenController rightScreenController;
-    @FXML private AirportConfigController airportConfigController;
 
-    @FXML private MenuBar menuBar;
+    @FXML
+    private MenuBar menuBar;
 
-    XMLParser xmlParser = new XMLParser();
-
-    @FXML private void initialize() {
+    @FXML
+    private void initialize() {
     }
 
-    @FXML private void importXML (ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Import XML");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("xml files" , "*.xml"));
-        File xmlFile = fileChooser.showOpenDialog(menuBar.getScene().getWindow());
-
-        /*
-         * Try/catch and if statements to check whether an airport or obstacle has been imported
-         * and if the import was successful
-         * TODO: Figure out how to update the choicebox to show the updated model list
-         */
-        try {
-            ArrayList<Airport> importedAirports = xmlParser.importAirports(xmlFile);
-            if (!importedAirports.isEmpty()) {
-                Model.airports = importedAirports;
-                Model.airportConfigController.update();
-                //Model.airportConfigController.emptyAirportsUpdate();
-                Model.console.addLog("--- Imported Airports and Runways ---");
-                for (Airport a : Model.airports)
-                {
-                    Model.console.addLog(a.toString());
-                    //TODO: this is really way of doing it, will change it during increment2
-                    Model.console.addLog(a.getRunways().toString());
-                }
-                if(Model.leftScreenController.accordion.getExpandedPane()!=null)
-                    Model.leftScreenController.accordion.getExpandedPane().setExpanded(false);
-                Model.console.addLog("--- Finished Importing ---");
-                Model.currentAirport = Model.getAirportByName(Model.airportConfigController.currentAirport.getName());
-                if(Model.currentAirport.getRunways() != null){}
-                    //Model.runwayConfigController.runwayMenuChanger();
-            }
-            else {
-                ArrayList<Obstacle> importedObstacles = xmlParser.importObstacle(xmlFile);
-                if (!importedObstacles.isEmpty()) {
-                    Model.obstacles = importedObstacles;
-                    Model.console.addLog("--- Imported Objects ---");
-                    for (Obstacle o : Model.obstacles)
-                    {
-                        Model.console.addLog(o.getName());
-                    }
-                    Model.console.addLog("--- Finished Importing ---");
-                }
-                else {
-                    throw new Exception();
-                }
-            }
-        }
-        catch (Exception e) {
-            Model.console.addLog("Failed an import");
-            Alert failedImport = new Alert(Alert.AlertType.WARNING, "Import Failed");
-            failedImport.setHeaderText(null);
-            failedImport.showAndWait();
-        }
+    @FXML
+    private void importXML(ActionEvent event) {
     }
 
     public void exportAirports(ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Export XML");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("xml files" , "*.xml"));
-        File xmlFile = fileChooser.showSaveDialog(menuBar.getScene().getWindow());
-
-        try {
-            xmlParser.exportAirports(xmlFile);
-        }
-        catch (Exception e) {
-            Alert failedExport = new Alert(Alert.AlertType.WARNING, "Failed to export the current airports");
-            failedExport.setHeaderText(null);
-            failedExport.showAndWait();
-        }
     }
 }
