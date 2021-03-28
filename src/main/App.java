aa08b1ee95b5fbe787048e611fb5b0e291fead14
Tomas Mrkva <22m.tomash@gmail.com>
2021-03-28 22:48:05 +0100

    text resizing added + improved UI readability

--- src/main/App.java
+++ src/main/App.java
@@ -4,17 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import main.model.Model;
 
 public class App extends Application {
+
+    public static Parent root = null;
+    public static Stage stage = null;
 
     public static void run(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
+    public void start(Stage s) throws Exception {
+        App.stage = s;
+        root = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setHeight(768);
@@ -23,6 +29,8 @@ public class App extends Application {
         stage.setMinWidth(850);
         stage.setTitle("Runway Re-declaration Tool");
         stage.show();
+        Model.centerScreenController.console.setFont(Font.font("Consolas",12.5));
+        Model.rightScreenController.changeFontSize(12.5);
     }
 
     @Override
--- src/main/controllers/RightScreenController.java
+++ src/main/controllers/RightScreenController.java
@@ -3,15 +3,17 @@ package main.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import main.model.LogicalRunWay;
 import main.model.Model;
 
 public class RightScreenController {
 
+    public TableView<LogicalRunWay> topTableView;
+    public TableView<LogicalRunWay> bottomTableView;
     public TextArea allCalculationsTowardsTA;
     public TextArea allCalculationsAwayTA;
-    @FXML private TableView<LogicalRunWay> topTableView;
-    @FXML private TableView<LogicalRunWay> bottomTableView;
     public TextArea toraTopTA;
     public TextArea toraBottomTA;
     public TextArea todaTopTA;
@@ -20,6 +22,7 @@ public class RightScreenController {
     public TextArea ldaBottomTA;
     public TextArea asdaTopTA;
     public TextArea asdaBottomTA;
+    public VBox rightScreen;
 
     public RightScreenController() {}
 
@@ -32,5 +35,18 @@ public class RightScreenController {
         topTableView.getItems().addAll(Model.originalRunwayTowards, Model.originalRunwayAway);
         bottomTableView.getItems().clear();
         bottomTableView.getItems().addAll(Model.recalculatedRunwayTowards, Model.recalculatedRunwayAway);
+    }
+
+    public void changeFontSize(double size){
+        allCalculationsTowardsTA.setFont(Font.font("Consolas", size));
+        allCalculationsAwayTA.setFont(Font.font("Consolas",size));
+        toraTopTA.setFont(Font.font("Consolas", size));
+        toraBottomTA.setFont(Font.font("Consolas",size));
+        todaTopTA.setFont(Font.font("Consolas",size));
+        todaBottomTA.setFont(Font.font("Consolas",size));
+        ldaTopTA.setFont(Font.font("Consolas",size));
+        ldaBottomTA.setFont(Font.font("Consolas",size));
+        asdaTopTA.setFont(Font.font("Consolas",size));
+        asdaBottomTA.setFont(Font.font("Consolas",size));
     }
 }
