7edd11eeaee7b3d69a7b935d383782ccba0668a0
Tomas Mrkva <22m.tomash@gmail.com>
2021-03-31 15:48:41 +0100

    added visualisations examples

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,15 +1,8 @@
 package test;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-class Main extends Application {
+class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        
-    }
 }
