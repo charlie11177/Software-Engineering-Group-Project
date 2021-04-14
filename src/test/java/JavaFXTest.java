4230dc813b2a39954d1401f9adc9bf062c158800
jm10g19 <jm10g19@soton.ac.uk>
2021-04-14 18:46:29 +0100

    TestFX working with a sample test

--- src/test/java/JavaFXTest.java
+++ src/test/java/JavaFXTest.java
@@ -1,25 +1,32 @@
-import app.App;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import model.Model;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testfx.api.FxAssert;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
 import java.io.File;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class JavaFXTest extends App {
+@ExtendWith(ApplicationExtension.class)
+public class JavaFXTest {
 
     private Scene scene;
 
-    @Override
+    @Start
     public void start(Stage stage) {
         try {
             URL url = new File("src/main/resources/view/MainWindow.fxml").toURI().toURL();
@@ -33,8 +40,20 @@ public class JavaFXTest extends App {
         }
     }
 
+    /**
+     * Tests that model gets updated when a new airport is added
+     * @param robot
+     */
     @Test
-    public void testTest(FxRobot robot) {
-        Assertions.assertThat(robot.lookup("#airportConfig").queryAs(TitledPane.class).isExpanded());
+    public void airportCreationTest(FxRobot robot) {
+        String name = "Southampton Airport";
+        String code = "SOU";
+        robot.clickOn("#airportConfig");
+        robot.clickOn("#newAirportButton");
+        robot.clickOn("#airportNameTextField").write(name);
+        robot.clickOn("#airportCodeTextField").write(code);
+        robot.clickOn("#saveAirport");
+        Assertions.assertThat(Model.airports.size()).isEqualTo(1);
     }
+
 }
