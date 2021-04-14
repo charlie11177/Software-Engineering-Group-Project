394e8a6c430d3f46d02ebafb372a836b0186a22f
VelinaValcheva <71768448+VelinaValcheva@users.noreply.github.com>
2021-04-14 20:37:37 +0300

    4 scenarios Calculators tests

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,8 +1,17 @@
 package test;
 
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
+class Main {
+
+    //    TestRunnerClass
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(CalculatorTest.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+//        System.out.println("Result==" + result.wasSuccessful());
+    }
 }
