9a3c180202d4417121ce0dda49894bf70e181647
vv1u19 <vv1u19@soton.ac.uk>
2021-04-23 09:56:56 +0000

    without fx StandardTests.java

--- src/test/java/StandardTests.java
+++ src/test/java/StandardTests.java
@@ -1,9 +1,11 @@
-//import org.junit.jupiter.api.*;
+import model.Model;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.testfx.assertions.api.Assertions.assertThat;
+
 //
-//import static org.junit.jupiter.api.Assertions.fail;
-//import static org.junit.jupiter.api.Assumptions.assumeTrue;
-//
-//class StandardTests {
+class StandardTests {
 //
 //    @BeforeAll
 //    static void initAll() {
@@ -13,9 +15,13 @@
 //    void init() {
 //    }
 //
-//    @Test
-//    void succeedingTest() {
-//    }
+    static class foo {
+        public static String test = "test";
+    }
+    @Test
+    void succeedingTest() {
+        assertThat(foo.test).isEqualTo("test");
+    }
 //
 //    @Test
 //    void failingTest() {
@@ -42,5 +48,4 @@
 //    static void tearDownAll() {
 //    }
 //
-//}
-//
+}
